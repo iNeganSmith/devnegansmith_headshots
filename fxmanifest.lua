1adbc134d7d75e7886ff8b38213651748ff1a869
RyuKazu <pacheco97j@gmail.com>
2026-09-20 10:53:36 -0500

    chore: prepare v1.0.2 for FiveM asset delivery

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,12 @@ lua54 'yes'
 name 'devnegansmith_headshots'
 author 'DevNeganSmith'
 description 'Headshots letales configurables. Standalone, compatible con ESX Legacy, QBCore y Qbox/QBX.'
-version '1.0.1'
+version '1.0.2'
 
 shared_script 'config.lua'
 client_script 'client.lua'
+
+-- Mantiene la configuración editable cuando el recurso se distribuye mediante FiveM Asset Escrow.
+escrow_ignore {
+    'config.lua'
+}
