fb1d1f0a1e088e2ddd20550609625ebea82641da
RyuKazu <pacheco97j@gmail.com>
2026-09-20 10:40:35 -0500

    chore: bump version to 1.0.1 after documentation update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 name 'devnegansmith_headshots'
 author 'DevNeganSmith'
 description 'Headshots letales configurables. Standalone, compatible con ESX Legacy, QBCore y Qbox/QBX.'
-version '1.0.0'
+version '1.0.1'
 
 shared_script 'config.lua'
 client_script 'client.lua'
